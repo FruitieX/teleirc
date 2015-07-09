9df49c17b171cada6c5b8e48685f84a4dffe32e1
Klaus Helenius <klaus.helenius@gmail.com>
2015-07-09 05:21:21 +0300

    Updated program to use Telegram Bot API

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ tg_irc
 
 Telegram <-> IRC gateway
 
-* Uses the [`telegram-cli`](https://github.com/vysheng/tg) program for Telegram communication
+* Uses [`node-telegram-bot`](https://github.com/orzFly/node-telegram-bot) Telegram bot API wrapper for Telegram communication
 * IRC communication via martynsmith's [`node-irc`](https://github.com/martynsmith/node-irc) module
 * All Telegram messages sent to IRC channel
 * IRC messages sent to Telegram only when bot is hilighted (configurable)
@@ -17,8 +17,4 @@ Setup
     $EDITOR ~/.tg_irc_config.js
     node tg_irc.js
 
-Note that before running `tg_irc`, you should have ran
-`telegram-cli` at least once and set up your account. Make
-sure you can actually send messages to group chats from within
-`telegram-cli` yourself, it has to work or else `tg_irc` won't
-be able to send IRC -> TG messages either!
+Note that before running `tg_irc`, you should create your Telegram bot first (https://core.telegram.org/bots#botfather)
--- tg_irc.js
+++ tg_irc.js
@@ -10,7 +10,7 @@ var config = require(process.env.HOME + '/.tg_irc_config.js');
 
 
 //////////////////
-//  IRC client  //
+//  IRC Client  //
 //////////////////
 
 var irc = require('irc');
@@ -20,66 +20,27 @@ client.on('error', function(error) {
     console.log('error: ', error);
 });
 
-
-
-//////////////////
-//  TG >>> IRC  //
-//////////////////
-
 var irc_send_msg = function(msg) {
     console.log('  >> relaying to IRC: ' + msg);
     client.say(config.irc_channel_id, msg);
 };
 
-var tg_handle_line = function(line) {
-    if(line.match(new RegExp('\\[\\d\\d:\\d\\d\\]  ' + config.tg_chat + ' .* >>> .*'))) {
-        console.log('TG: ' + line);
-        line = line.split(' ');
-        line.shift(); line.shift(); line.shift();
-        // [Firstname, Lastname, ..., >>>, msgword1, msgword2, ...]
 
-        var name = '';
-        temp = line.shift();
-        while(temp !== '>>>') {
-            name += temp;
-            temp = line.shift();
-        }
 
-        line = line.join(' ');
+//////////////////
+//  TG bot API  //
+//////////////////
 
-        if(name === config.tg_nick)
-        //if(line.indexOf('irc: ') === 0)
-            return;
+var telegram = require('telegram-bot');
+var tg = new telegram(config.tg_bot_token);
+tg.start();
 
-        irc_send_msg('<' + name + '>: ' + line);
-    }
+var tg_send_msg = function(msg) {
+    tg.sendMessage({
+        text: msg,
+        chat_id: config.tg_chat_id
+    });
 };
-
-
-
-///////////////////
-//  TG listener  //
-///////////////////
-
-var spawn = require('child_process').spawn;
-var telegram = spawn(config.tg_cli_path, ['-R', '-C', '-W', '-k', config.tg_pubkey_path]);
-var stdoutBuf = '';
-telegram.stdout.on('data', function(data) {
-    stdoutBuf += data.toString('utf8');
-    var lastNL = stdoutBuf.lastIndexOf('\n');
-
-    // have we received at least one whole line? else wait for more data
-    if(lastNL !== -1) {
-        var recvdLines = stdoutBuf.substr(0, lastNL + 1).split('\n');
-        stdoutBuf = stdoutBuf.substr(lastNL + 1);
-
-        for(var i = 0; i < recvdLines.length; i++) {
-            if(recvdLines[i] !== '') {
-                tg_handle_line(recvdLines[i]);
-            }
-        }
-    }
-});
 
 
 
@@ -88,13 +49,25 @@ telegram.stdout.on('data', function(data) {
 //////////////////
 
 client.on('message', function(user, channel, message) {
-    console.log('IRC: ' + user + ': ' + message);
+    //console.log('IRC: ' + user + ': ' + message);
     var match = config.irc_hilight_re.exec(message);
     if (match || config.irc_relay_all) {
         if (match)
             message = match[1].trim();
-        var tg_msg = 'msg ' + config.tg_chat + ' <' + user + '>: ' + message;
+        var tg_msg = '<' + user + '>: ' + message;
         console.log('  >> relaying to TG: ' + tg_msg);
-        telegram.stdin.write(tg_msg + '\n');
+        tg_send_msg(tg_msg);
     }
+});
+
+
+
+//////////////////
+//  TG >>> IRC  //
+//////////////////
+
+tg.on('message', function(msg) {
+    if (!msg.text) return;
+    var text = '<' + msg.from.first_name + msg.from.last_name + '>: ' + msg.text;
+    irc_send_msg(text);
 });
