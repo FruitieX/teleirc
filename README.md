56410a59d42a1afc12fd21e25cf5cc2349d2df3e
Klaus Helenius <klaus.helenius@aalto.fi>
2015-08-27 04:33:08 +0300

    added support for multiple irc channel <-> tg chat pairs

--- README.md
+++ README.md
@@ -3,18 +3,18 @@ tg_irc
 
 Telegram <-> IRC gateway
 
-* Uses [`node-telegram-bot`](https://github.com/orzFly/node-telegram-bot) Telegram bot API wrapper for Telegram communication
-* IRC communication via martynsmith's [`node-irc`](https://github.com/martynsmith/node-irc) module
-* All Telegram messages sent to IRC channel
-* IRC messages sent to Telegram only when bot is hilighted (configurable)
+* Telegram communication via [node-telegram-bot](https://github.com/orzFly/node-telegram-bot) library
+* IRC communication via martynsmith's [node-irc](https://github.com/martynsmith/node-irc) module
+* All Telegram messages are sent to IRC channel
+* IRC messages sent to Telegram only when bot is hilighted or if message is prefixed with "! " (configurable)
 
 Setup
 -----
 
     git clone https://github.com/warbaque/tg_irc && cd tg_irc
     npm install
-    cp tg_irc_config.js.example ~/.tg_irc_config.js
-    $EDITOR ~/.tg_irc_config.js
-    node tg_irc.js
+    mkdir ~/.tg_irc && cp tg_irc_config.js.example ~/.tg_irc/config.js
+    $EDITOR ~/.tg_irc/config.js
+    npm start
 
 Note that before running `tg_irc`, you should create your Telegram bot first (https://core.telegram.org/bots#botfather)
--- tg_irc.js
+++ tg_irc.js
@@ -1,83 +1,155 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+var nodeirc = require('irc');
+var telegram = require('telegram-bot');
 
-//////////////
-//  Config  //
-//////////////
 
-var config = require(process.env.HOME + '/.tg_irc_config.js');
+/////////////////////////
+//  Config and helpers //
+/////////////////////////
 
+
+var config = require(process.env.HOME + '/.tg_irc/config.js');
+
+// channel option lookup
+var lookup = function (type, channel, arr) {
+    return arr.filter(function (obj) {
+        return obj[type] === channel;
+    })[0];
+}
+
+// generates channel list for irc_options
+var get_channels = function (arr) {
+    var result = [];
+    for (var i=0; i<arr.length; ++i) {
+        var channel = arr[i].irc_channel_pwd ?
+            arr[i].irc_channel + ' ' + arr[i].irc_channel_pwd :
+            arr[i].irc_channel;
+        result.push(channel);
+    }
+    return result;
+}
+
+// tries to read chat ids from a file
+var get_chat_ids = function (arr) {
+    var result = {};
+    // try to read tg_chat_ids from disk
+    // otherwise get it from first conversation
+    console.log('\n');
+    console.log('NOTE!');
+    console.log('=====');
+    var id_missing = false;
+    try {
+        var json = JSON.parse(fs.readFileSync(process.env.HOME + '/.tg_irc/chat_ids'));
+        for (var i=0; i<arr.length; ++i) {
+            var key = arr[i].tg_chat;
+            if (key in json) {
+                result[key] = json[key];
+                console.log('id found for:', key, ':', json[key]);
+            }
+            else {
+                console.log('id not found:', key);
+                id_missing = true;
+            }
+        }
+    } catch(e) {
+        console.log('~/.tg_irc/chat_ids file not found!');
+        id_missing = true;
+    }
+    if (id_missing) {
+        console.log(
+            '\nPlease add your Telegram bot to a Telegram group and have' +
+            '\nsomeone send a message to that group.' +
+            '\ntg_irc will then automatically store your group chat_id.');
+    }
+    console.log('\n');
+    return result;
+}
+
+config.irc_options.channels = get_channels(config.channels);
+tg_chat_ids = get_chat_ids(config.channels);
 
 
 //////////////////
 //  IRC Client  //
 //////////////////
 
-var irc = require('irc');
-var client = new irc.Client(config.irc_server, config.irc_nick, config.irc_options);
+var irc = new nodeirc.Client(config.irc_server, config.irc_nick, config.irc_options);
 
-client.on('error', function(error) {
+irc.on('error', function(error) {
     console.log('error: ', error);
 });
 
-var irc_send_msg = function(msg) {
+var irc_send_msg = function(irc_channel, msg) {
     console.log('  >> relaying to IRC: ' + msg);
-    client.say(config.irc_channel_id, msg);
+    irc.say(irc_channel, msg);
 };
-
 
 
 //////////////////
 //  TG bot API  //
 //////////////////
 
-var telegram = require('telegram-bot');
 var tg = new telegram(config.tg_bot_token);
 tg.start();
 
-var tg_send_msg = function(msg) {
+var tg_send_msg = function(tg_chat_id, msg) {
     console.log('  >> relaying to TG: ' + msg);
     tg.sendMessage({
         text: msg,
-        chat_id: config.tg_chat_id
+        chat_id: tg_chat_id
     });
 };
-
 
 
 //////////////////
 //  IRC >>> TG  //
 //////////////////
 
-client.on('message', function(user, channel, message) {
-    if (config.irc_channel_id !== channel)
+irc.on('message', function(user, channel, message) {
+    var conf = lookup('irc_channel', channel, config.channels);
+    if (!conf) {
         return;
-    //console.log('IRC: ' + user + ': ' + message);
+    }
+
     var match = config.irc_hilight_re.exec(message);
     if (match || config.irc_relay_all) {
-        if (match)
+
+        if (!tg_chat_ids[conf.tg_chat]) {
+            var err = 'Error: No chat_id set! Add me to a Telegram group '
+                    + 'and say hi so I can find your chat_id!';
+            irc_send_msg(conf.irc_channel, err);
+            console.log(err);
+            return;
+        }
+
+        if (match) {
             message = match[1].trim();
-        var tg_msg = '<' + user + '>: ' + message;
-        tg_send_msg(tg_msg);
+        }
+        var text = '<' + user + '>: ' + message;
+        tg_send_msg(tg_chat_ids[conf.tg_chat], text);
     }
 });
 
 // ignore first topic event when joining channel
 var first_topic_event = true;
-client.on('topic', function(channel, topic, nick)
-{
+irc.on('topic', function(channel, topic, nick) {
+    var conf = lookup('irc_channel', channel, config.channels)
+    if (!conf) {
+        return;
+    }
+
     if (first_topic_event) {
         first_topic_event = false;
         return;
     }
-    if (config.irc_channel_id !== channel)
-        return;
-    var tg_msg  = '* Topic for channel ' + config.irc_channel.split(' ')[0]
-                + ':\n' + topic.split(' | ').join('\n')
-                + '\n* set by ' + nick.split('!')[0];
-    tg_send_msg(tg_msg);
-});
 
+    var text = '* Topic for channel ' + config.irc_channel.split(' ')[0]
+             + ':\n' + topic.split(' | ').join('\n')
+             + '\n* set by ' + nick.split('!')[0];
+    tg_send_msg(conf.tg_chat, text);
+});
 
 
 //////////////////
@@ -85,14 +157,32 @@ client.on('topic', function(channel, topic, nick)
 //////////////////
 
 tg.on('message', function(msg) {
-    if (config.tg_chat !== msg.chat.title)
+    var conf = lookup('tg_chat', msg.chat.title, config.channels);
+    if (!conf) {
         return;
-    console.log('chat_id: ' + msg.chat.id);
-    config.tg_chat_id = msg.chat.id;
-    if (!msg.text)
+    }
+
+    if (!tg_chat_ids[conf.tg_chat]) {
+        tg_chat_ids[conf.tg_chat] = msg.chat.id;
+
+        console.log('storing chat ID: ' + msg.chat.id);
+        var json = JSON.stringify(tg_chat_ids);
+        fs.writeFile(process.env.HOME + '/.tg_irc/chat_ids', json, function(err) {
+            if (err) {
+                console.log('error while storing chat ID:');
+                console.log(err);
+            } else {
+                console.log('successfully stored chat ID in ~/.tg_irc/chat_ids');
+            }
+        });
+    }
+
+    if (!msg.text) {
         return;
-    var user    = msg.from.first_name ? msg.from.first_name : ''
-                + msg.from.last_name ? msg.from.last_name : '';
+    }
+
+    var user = msg.from.first_name ? msg.from.first_name : ''
+             + msg.from.last_name ? msg.from.last_name : '';
     var text = '<' + user + '>: ' + msg.text;
-    irc_send_msg(text);
+    irc_send_msg(conf.irc_channel, text);
 });
