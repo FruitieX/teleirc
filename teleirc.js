6076292a22234ce2440a7218b493db1609a9bf4a
Klaus Helenius <klaus.helenius@aalto.fi>
2015-08-30 21:12:14 +0300

    fixed topic events

--- teleirc.js
+++ teleirc.js
@@ -81,9 +81,9 @@ irc.on('error', function(error) {
     console.log('error: ', error);
 });
 
-var irc_send_msg = function(irc_channel, msg) {
+var irc_send_msg = function(irc_channel_id, msg) {
     console.log('  >> relaying to IRC: ' + msg);
-    irc.say(irc_channel, msg);
+    irc.say(irc_channel_id, msg);
 };
 
 
@@ -94,11 +94,20 @@ var irc_send_msg = function(irc_channel, msg) {
 var tg = new telegram(config.tg_bot_token);
 tg.start();
 
-var tg_send_msg = function(tg_chat_id, msg) {
+var tg_send_msg = function(conf, msg) {
     console.log('  >> relaying to TG: ' + msg);
+
+    if (!tg_chat_ids[conf.tg_chat]) {
+        var err = 'Error: No chat_id set! Add me to a Telegram group '
+                + 'and say hi so I can find your chat_id!';
+        irc_send_msg(conf.irc_channel_id, err);
+        console.log(err);
+        return;
+    }
+
     tg.sendMessage({
         text: msg,
-        chat_id: tg_chat_id
+        chat_id: tg_chat_ids[conf.tg_chat]
     });
 };
 
@@ -108,47 +117,38 @@ var tg_send_msg = function(tg_chat_id, msg) {
 //////////////////
 
 irc.on('message', function(user, channel, message) {
-    var conf = lookup('irc_channel', channel, config.channels);
+    var conf = lookup('irc_channel_id', channel, config.channels);
     if (!conf) {
         return;
     }
 
     var match = config.irc_hilight_re.exec(message);
     if (match || config.irc_relay_all) {
-
-        if (!tg_chat_ids[conf.tg_chat]) {
-            var err = 'Error: No chat_id set! Add me to a Telegram group '
-                    + 'and say hi so I can find your chat_id!';
-            irc_send_msg(conf.irc_channel, err);
-            console.log(err);
-            return;
-        }
-
         if (match) {
             message = match[1].trim();
         }
         var text = '<' + user + '>: ' + message;
-        tg_send_msg(tg_chat_ids[conf.tg_chat], text);
+        tg_send_msg(conf, text);
     }
 });
 
-// ignore first topic event when joining channel
-var first_topic_event = true;
 irc.on('topic', function(channel, topic, nick) {
-    var conf = lookup('irc_channel', channel, config.channels)
+    var conf = lookup('irc_channel_id', channel, config.channels)
     if (!conf) {
         return;
     }
 
-    if (first_topic_event) {
-        first_topic_event = false;
+    // ignore first topic event when joining channel
+    // (doesn't handle rejoins yet)
+    if (!conf.send_topic) {
+        conf.send_topic = true;
         return;
     }
 
-    var text = '* Topic for channel ' + config.irc_channel.split(' ')[0]
+    var text = '* Topic for channel ' + conf.irc_channel
              + ':\n' + topic.split(' | ').join('\n')
              + '\n* set by ' + nick.split('!')[0];
-    tg_send_msg(conf.tg_chat, text);
+    tg_send_msg(conf, text);
 });
 
 
@@ -184,5 +184,5 @@ tg.on('message', function(msg) {
     var user = msg.from.first_name ? msg.from.first_name : ''
              + msg.from.last_name ? msg.from.last_name : '';
     var text = '<' + user + '>: ' + msg.text;
-    irc_send_msg(conf.irc_channel, text);
+    irc_send_msg(conf.irc_channel_id, text);
 });
