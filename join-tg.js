70e663f4d4794c3a2048d07486f23cb7955e2645
Rasmus Eskola <fruitiex@gmail.com>
2016-04-05 10:29:23 +0300

    group is upgraded to supergroup and needs new invite URL...

--- join-tg.js
+++ join-tg.js
@@ -8,7 +8,7 @@ console.log('Join our official Telegram group:\n');
 
 // generated with new Buffer(theUrl, 'ascii').toString('hex')
 var h = '68747470733a2f2f74656c656772616d2e6d652f6a6f696e636' +
-        '861742f4151574b4a776649486f62774d386541645467796e41';
+        '861742f4151574b4a7a355a483563546f644d746f6d445f3741';
 var url = new Buffer(h, 'hex').toString('ascii');
 
 console.log(url);
