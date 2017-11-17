12519c84edad77d26700accc4d0e3e203d79c3a2
Rasmus Eskola <fruitiex@gmail.com>
2017-11-17 15:39:10 +0200

    Use tgfancy instead of node-telegram-bot-api

--- src/tg/index.js
+++ src/tg/index.js
@@ -1,4 +1,4 @@
-var Telegram = require('node-telegram-bot-api');
+var Telegram = require('tgfancy');
 var config = require('../config');
 var tgUtil = require('./util');
 var logger = require('winston');
@@ -11,7 +11,10 @@ var init = function(msgCallback) {
         tgUtil.initHttpServer();
     }
 
-    var tg = new Telegram(config.tgToken, {polling: true});
+    var tg = new Telegram(config.tgToken, {
+        polling: true,
+        emojification: true,
+    });
 
     // get our own Telegram user
     tg.getMe().then(function(me) {
